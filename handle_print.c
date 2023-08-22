8b0eb1ca376ed26751c9d2e40a841e38d8ac73ac
ashleynatalie <njugunaashley@gmail.com>
2023-08-22 13:21:46 -0700

    Collaboration project

--- handle_print.c
+++ handle_print.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument basing on type
- * @fmt: String in which to print arguments.
- * @list: List of arguments to print.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
  * @ind: ind.
- * @buffer: Buffer array to handle printing.
- * @flags: finding active flags
- * @width: gets the width
- * @precision: Precision specification in arguments
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-			int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -22,13 +22,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-<<<<<<< HEAD
-
-	for (int i = 0; fmt_types[i].fmt != '\0'; i++)
-	{
-=======
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
->>>>>>> 6409bc3b21f5e8bc2d2f70d2ab9d4644e39fbdf9
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
